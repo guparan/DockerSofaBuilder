ee4b81bbecae563e1bc0d7cecbc4c839f065f8e5
Guillaume Paran <guillaume.paran@sofa-framework.org>
2020-01-30 11:35:52 +0100

    FIX VS install with correct timeouts

--- sofabuilder_windows_vs2017/wait_process_to_end.bat
+++ sofabuilder_windows_vs2017/wait_process_to_end.bat
@@ -3,8 +3,8 @@
 set arg1=%1
 
 :loop
-tasklist.exe | findstr %arg1%
+tasklist.exe | findstr %arg1% > nul
 if "%ERRORLEVEL%"=="1" exit /b 0
 echo %arg1% is still running ...
-ping -n 5 127.0.0.1
+ping -n 5 127.0.0.1 > nul
 goto loop
